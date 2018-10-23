515cdfb4188df284dd757a3cd21daeb7f7cf9f50
Navar <navarmn@gmail.com>
2018-10-23 17:31:53 -0300

    gitignore now working

---
+++ 01_container_with_postgres/create_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY, 
+    company VARCHAR(50),
+    city VARCHAR(50), 
+    uf VARCHAR(2),
+    product_name VARCHAR(50)
+    );
+
+
+INSERT INTO products (company, city, uf, product_name)
+VALUES ('Empresa_Pedro', 'Fortaleza', 'CE', 'Cachaça') RETURNING id;
+
+INSERT INTO products (company, city, uf, product_name)
+VALUES ('Empresa_Navar', 'Fortaleza', 'CE', 'Whey') RETURNING id;
+
+INSERT INTO products (company, city, uf, product_name)
+VALUES ('Empresa_Raul', 'Fortaleza', 'CE', 'Pipoca') RETURNING id;
